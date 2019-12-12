f1f9594e08aef24200f41c61965bfa694e734c59
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-12 16:35:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,13 @@
+
+
 def oxford_comma(array)
   if array.size == 1 
     return array[0]
   elsif array.size == 2 
-    return "" + array[0] + " and " +array[1] 
+    return "" + array[0] + " and " +array[1]
+  else 
+    str = array.join(", ")
+    last = str.pop()
+    
   end
 end
